a79580dd359ca743e8a5b1e0a0b058ef41a9f15e
ackmanx <ackmanx@users.noreply.github.com>
2023-12-09 17:19:17 -0600

    Example with marked working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { marked } from 'marked';
+	import { onMount } from 'svelte';
+
+  let markdown
+
+	onMount(() => {
+    markdown = marked.parse(
+      '# Marked in the browser\n\nRendered by **marked**.'
+    )
+  })
+</script>
+
+<svelte:head>
+  <title>Mmmmm</title>
+</svelte:head>
+
+<main>{@html markdown}</main>
