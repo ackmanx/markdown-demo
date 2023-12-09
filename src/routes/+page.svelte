3d074684f253d68d89cc6da754b9435c96341b5f
ackmanx <ackmanx@users.noreply.github.com>
2023-12-09 17:51:14 -0600

    Try using prism for syntax highlighting but I think i'll find a different solution instead that combines a textarea and highlighting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,52 @@
 <script>
 	import { marked } from 'marked';
 	import { onMount } from 'svelte';
+	import './prism.js';
 
-	let markdown;
+	let htmlPreview;
+	let markdown = '# Marked in the browser\n\nRendered by **marked**.';
 
 	onMount(() => {
-		markdown = marked.parse('# Marked in the browser\n\nRendered by **marked**.');
+		htmlPreview = marked.parse(markdown);
+		Prism.highlightAll();
 	});
+
+	function handleTextInput(event) {
+		markdown = event.currentTarget.value;
+		htmlPreview = marked.parse(markdown);
+		Prism.highlightAll();
+	}
 </script>
 
 <svelte:head>
 	<title>Mmmmm</title>
-</svelte:head>
 
-<style>
-  h2.header {
-    color: red;
-  }
-</style>
+	<script>
+		window.Prism = window.Prism || {};
+		window.Prism.manual = true;
+	</script>
+</svelte:head>
 
 <main>
 	<div>
 		<h2 class="header">Input</h2>
-		<textarea></textarea>
+		<textarea cols="80" rows="15" on:input={handleTextInput}>{markdown}</textarea>
+		<div>
+			<h3>Hmm the highlighted version only works on first render</h3>
+			<pre><code class="language-markdown">{markdown}</code></pre>
+			<pre><code>{markdown}</code></pre>
+		</div>
 	</div>
 	<div>
 		<h2 class="header">Preview</h2>
-		{@html markdown}
+		{@html htmlPreview}
 	</div>
 </main>
+
+<style>
+	@import './prism.css';
+
+	h2.header {
+		color: red;
+	}
+</style>
