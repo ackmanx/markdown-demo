7dffae6d1f61835c4526d15bb7b1681903a80843
ackmanx <ackmanx@users.noreply.github.com>
2023-12-09 17:23:28 -0600

    Working towards text area updating preview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,30 @@
 	import { marked } from 'marked';
 	import { onMount } from 'svelte';
 
-  let markdown
+	let markdown;
 
 	onMount(() => {
-    markdown = marked.parse(
-      '# Marked in the browser\n\nRendered by **marked**.'
-    )
-  })
+		markdown = marked.parse('# Marked in the browser\n\nRendered by **marked**.');
+	});
 </script>
 
 <svelte:head>
-  <title>Mmmmm</title>
+	<title>Mmmmm</title>
 </svelte:head>
 
-<main>{@html markdown}</main>
+<style>
+  h2.header {
+    color: red;
+  }
+</style>
+
+<main>
+	<div>
+		<h2 class="header">Input</h2>
+		<textarea></textarea>
+	</div>
+	<div>
+		<h2 class="header">Preview</h2>
+		{@html markdown}
+	</div>
+</main>
