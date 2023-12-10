fff59720b0cf2df8531e4925a46cafac3de9a405
ackmanx <ackmanx@users.noreply.github.com>
2023-12-10 00:09:46 -0600

    Oh yeah CodeMirror is pretty cool

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,31 @@
 	import { marked } from 'marked';
 	import { onMount } from 'svelte';
 
+	import { basicSetup, EditorView } from 'codemirror';
+	import { markdown } from '@codemirror/lang-markdown';
+	import { languages } from '@codemirror/language-data';
+	import { indentWithTab } from '@codemirror/commands';
+	import { keymap } from '@codemirror/view';
+
 	let htmlPreview;
-	let markdown = '# Marked in the browser\n\nRendered by **marked**.';
+	let markdownInput = '# Marked in the browser\n\nRendered by **marked**.';
 
 	onMount(() => {
-		htmlPreview = marked.parse(markdown);
+		htmlPreview = marked.parse(markdownInput);
+
+		// The Markdown parser will dynamically load parsers
+		// for code blocks, using @codemirror/language-data to
+		// look up the appropriate dynamic import.
+		let view = new EditorView({
+			doc: "Hello\n\n```javascript\nlet x = 'y'\n```",
+			extensions: [basicSetup, keymap.of([indentWithTab]), markdown({ codeLanguages: languages })],
+			parent: document.body
+		});
 	});
 
 	function handleTextInput(event) {
-		markdown = event.currentTarget.value;
-		htmlPreview = marked.parse(markdown);
+		markdownInput = event.currentTarget.value;
+		htmlPreview = marked.parse(markdownInput);
 	}
 </script>
 
@@ -22,12 +37,15 @@
 <main>
 	<div>
 		<h2 class="header">Input</h2>
-		<textarea cols="80" rows="15" on:input={handleTextInput}>{markdown}</textarea>
+		<textarea cols="80" rows="15" on:input={handleTextInput}>{markdownInput}</textarea>
 	</div>
 	<div>
 		<h2 class="header">Preview</h2>
 		{@html htmlPreview}
 	</div>
+
+	<h2 class="header">CodeMirror</h2>
+<!-- CodeMirror editor automatically goes here. Gotta figure out how to make it more explicit	-->
 </main>
 
 <style>
